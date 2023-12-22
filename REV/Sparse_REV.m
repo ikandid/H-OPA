6fa4522bb6f0d6ace13fa74a29e88a96219fedcf
Ilyas Kandid <ilyaskandid@cmail.carleton.ca>
2023-12-22 18:49:53 -0500

    Added optimization point

--- REV/Sparse_REV.m
+++ REV/Sparse_REV.m
@@ -56,3 +56,4 @@ ylabel('Phase (rad)')
 [Intensity_norm_theta,Intensity_dB_theta,p,theta,c,index,BW_3dB_theta,SLL_theta]=theta_cut(1,1,1,length(pos_final),res,0,pos_final,1,theta_0,ant,theta_90,x0);
 
 %% Determine the optimization point
+Opt = find(theta == 0);
