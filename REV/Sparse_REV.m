7e81617a1e3c3a1a84f01cc0d6262cf59570ba69
Kandid  Ilyas <ILYASKANDID>
2024-01-29 11:40:43 -0500

    Minor changes to test random phase offsets.

--- REV/Sparse_REV.m
+++ REV/Sparse_REV.m
@@ -7,7 +7,7 @@ c = 3e8;
 fc = 193e12;
 lambda = c/fc;
 k=2*pi/lambda;%wavenumber
-theta_0=2.5; %theta steering angle
+theta_0=0; %theta steering angle
 phi_0=0; %phi steering angle
 theta_90=1; %0 for 0:90/ 1 for -90:90
 ant = 0; %0 for isotropic antenna/ 1 for selected antenna
@@ -34,6 +34,12 @@ text(pos_final(1,:)'/1e-6,pos_final(2,:)'/1e-6,label);
 
 %Phase Offset
 phase_off = zeros(1,length(pos_final)); %no phase offset
+for i = 1:3:length(phase_off)
+    phase_off(i) = 2*pi*rand;
+end
+
+load('rand2_phase_off.mat')
+%phase_off = phase_steer;
 
 %define the AF and Intensity distributions
 %[Intensity_norm,Intensity_dB,Intensity_max,u,v,theta,phi]=AF_general(A,B,C,D,pos_final,lambda,figure_on_off,theta_0,phi_0,ant,theta_90,phase_off)
@@ -42,7 +48,7 @@ phase_off = zeros(1,length(pos_final)); %no phase offset
 %% 2D Array factor caclulation
 %theta cut
 %[Intensity_norm_theta,Intensity_dB_theta,p,theta,c,index,BW_3dB_theta,SLL_theta,w]=theta_cut(A,B,C,D,resolution,p,pos_final,figure_on_off,theta_0,ant,theta_90,phase_off)
-res = 100;
+res = 1;
 [Intensity_norm_theta,Intensity_dB_theta,p,theta,c,index,BW_3dB_theta,SLL_theta,nulls]=theta_cut(1,1,1,length(pos_final),res,0,pos_final,1,theta_0,ant,theta_90,phase_off);
 
 %phase offset figure
